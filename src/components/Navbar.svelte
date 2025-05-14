f8846688b80ede09f9a51667eb833d54b5ab694b
ikhlasdansantai <initesting5@gmail.com>
2025-05-14 17:17:44 +0700

    feat: add navbar component

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<nav
+	class="bg-neutral-0 flex items-center justify-between rounded-2xl border-neutral-200 p-4 shadow-md"
+>
+	<div class="logo__container">
+		<img src="/images/logo.svg" alt="logo-img" class="block max-w-full" />
+	</div>
+
+	<button class="block cursor-pointer rounded-lg bg-neutral-200 hover:bg-neutral-300">
+		<figure class="p-3">
+			<img src="/images/icon-moon.svg" alt="logo-img" class="block max-w-full" />
+		</figure>
+	</button>
+</nav>
