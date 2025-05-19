c4fc4419b4336787b8066e75418fa9cd660d9a06
ikhlasdansantai <initesting5@gmail.com>
2025-05-19 14:16:43 +0700

    feat: Add ExtensionCard component

--- src/components/ExtensionCard.svelte
+++ src/components/ExtensionCard.svelte
@@ -1,1 +1,56 @@
-<h2>Ini adalah component svelte</h2>
+<div
+	class="h-fit min-h-[min-content] max-w-[25rem] rounded-3xl border border-neutral-200 bg-white p-4 shadow-sm
+    sm:shadow-md
+    "
+>
+	<div class="ext__card item-start grid grid-cols-[4rem_1fr] justify-between gap-4">
+		<figure class="overflow-hidden rounded-md">
+			<img src="/images/logo-devlens.svg" class="block aspect-square max-w-full" alt="dummy-img" />
+		</figure>
+		<div class="titles space-y-2">
+			<h5 class="text-xl leading-6 font-bold">Devlens</h5>
+			<p class="text-neutral-500">Quickly inspect page layouts and visualize element boundaries.</p>
+		</div>
+	</div>
+	<div class="ext__card__footer mt-10 flex items-center justify-between gap-4">
+		<button
+			class="hover:text-neutral-0 cursor-pointer rounded-full border border-neutral-200 p-1 px-3 shadow-xs transition-colors hover:bg-red-600"
+		>
+			Remove
+		</button>
+		<label class="inline-flex cursor-pointer items-center">
+			<input type="checkbox" value="" class="peer sr-only" />
+			<div
+				class="
+            peer
+            ease-[cubic-bezier(0.4, 0, 0.2, 1)]
+            after:ease-[cubic-bezier(0.4, 0, 0.2, 1)]
+            relative
+            h-6
+            w-11
+            rounded-full
+            bg-gray-200
+            transition
+            peer-checked:bg-red-700
+            peer-focus:ring-4
+            peer-focus:ring-neutral-100
+            peer-focus:outline-none
+            after:absolute
+            after:start-[2px]
+            after:top-[2px]
+            after:h-5
+            after:w-5
+            after:rounded-full
+            after:border
+            after:border-gray-300
+            after:bg-white
+            after:transition-all
+            after:content-['']
+            peer-checked:after:translate-x-full
+            peer-checked:after:border-white
+            dark:bg-neutral-600"
+				aria-hidden="true"
+			></div>
+		</label>
+	</div>
+</div>
